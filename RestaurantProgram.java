47f08f089cc347789f3c57cdbf070d141a090129
Chris Kline <104172499+Ckliner@users.noreply.github.com>
2023-08-19 15:42:28 -0400

    Add files via upload

---
+++ RestaurantProgram.java
@@ -0,0 +1,16 @@
+
+public class RestaurantProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String Restaurant_Name = "Rayzor's Dawg House";
+		String Street_Address = "8178 N. Main St.";
+		String City_State_Zip = "Eden, New York 14057";
+		
+		System.out.println("Restaurant Name: " + Restaurant_Name);
+		System.out.println("Business Address: " + Street_Address);
+		System.out.println("Business City, State and Zip Code: " + City_State_Zip);
+	} 
+
+}
